15de0b5d7826ae8ed9a0e90b5c6a07610c2089ad
HentaiPlay <david.tonoyan16@mail.ru>
2024-02-12 00:04:59 +0300

    feat(pages): Добавлена домашняя страница

---
+++ src/pages/home/HomePage.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template lang="html">
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <div>Домашняя страница</div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style lang="scss" scoped></style>
