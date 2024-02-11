7297082f544468bc407198be7653eb1390630183
HentaiPlay <david.tonoyan16@mail.ru>
2024-02-11 21:20:27 +0300

    chore(vite): Добавлены alias-ы, добавлен checker для tsc, порт приложения и TODO для eslintPlugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
+// TODO: есть ошибка с опредлением типов для плагина
+// https://github.com/gxmari007/vite-plugin-eslint/issues/74
+// import eslintPlugin from 'vite-plugin-eslint'
+import checker from 'vite-plugin-checker'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  server: {
+    port: 5000
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      find: '@vue/runtime-core',
+      replacement: '@vue/runtime-core/dist/runtime-core.esm-bundler.js'
+    },
+    extensions: ['.js', '.ts', '.vue']
+  },
+  plugins: [
+    vue(),
+    // https://github.com/gxmari007/vite-plugin-eslint/issues/74
+    // eslintPlugin({ cache: false }),
+    checker({
+      vueTsc: true
+    })
+  ]
 })
