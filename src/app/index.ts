403ad53a1798338064cb6bb5f7d8eae845e720e0
HentaiPlay <david.tonoyan16@mail.ru>
2024-02-12 05:58:14 +0300

    feat(app): Подключен i18n в инстанс приложения

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { router, store, vuetify } from './providers'
+import { router, store, i18n, vuetify } from './providers'
 
-export const app = createApp(App).use(router).use(store).use(vuetify)
+export const app = createApp(App).use(router).use(store).use(i18n).use(vuetify)
