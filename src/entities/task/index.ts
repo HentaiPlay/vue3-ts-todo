856a41fec4c2d227cabc378452b4a6f45622c4d5
HentaiPlay <david.tonoyan16@mail.ru>
2024-02-12 16:00:51 +0300

    feat(task): Сервис, стор и компоненты импортированы в пространство имен сущности task

---
+++ src/entities/task/index.ts
@@ -0,0 +1,6 @@
+import { useTaskStore } from './model/store'
+import TaskService from './model/service'
+import TaskList from './ui/TaskList.vue'
+import TaskItem from './ui/TaskItem.vue'
+import TaskForm from './ui/TaskForm.vue'
+export { useTaskStore, TaskService, TaskList, TaskItem, TaskForm }
