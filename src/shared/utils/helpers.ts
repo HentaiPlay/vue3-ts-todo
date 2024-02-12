28dc2444502b7dcd27842c116dfe430ae34446da
HentaiPlay <david.tonoyan16@mail.ru>
2024-02-12 15:46:57 +0300

    feat(shared): Добавлен хэлпер с методом формирования уникальных ID

---
+++ src/shared/utils/helpers.ts
@@ -0,0 +1,10 @@
+// Генерация уникального ID
+export function generateUUID(): string {
+  let dateTime = new Date().getTime()
+  const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    const r = (dateTime + Math.random() * 16) % 16 | 0
+    dateTime = Math.floor(dateTime / 16)
+    return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16)
+  })
+  return uuid
+}
